1005ec48ccea378634764cae79ddb5a1637b1def
Simone Maurizio La Cava <simonemaurizio.lacava@gmail.com>
2022-01-02 16:36:45 +0100

    Connectivity metrics added

---
+++ magnitude_squared_coherence.m
@@ -0,0 +1,24 @@
+%% magnitude_squared_coherence
+% This function computes the averaged magnitude squared coherence between 
+% the signals of different channels or ROIs in the input matrix.
+%
+% MSC = magnitude_squared_coherence(sig)
+%
+% INPUT:
+%   sig is the input matrix (in the format time*locations)
+%
+% OUTPUT:
+%   PLI is the matrix of the magnitude squared coherence
+
+function MSC = magnitude_squared_coherence(sig)
+
+    nLoc = size(sig, 2);
+    MSC = zeros(nLoc, nLoc);
+
+    for i = 1:nLoc-1
+        for j = i+1:nLoc
+            MSC(i, j) = mean(mscohere(sig(:, i), sig(:, j)));    
+            MSC(j, i) = MSC(i, j);     
+        end
+    end
+end
